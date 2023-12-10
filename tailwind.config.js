e459e8fdf057639cff746a4ae8244cc54fc548d8
savsalmon <savannah.b.hilton@gmail.com>
2023-12-10 00:36:45 -0700

    got tailwind styles going

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Lexend: ["Lexend Deca", "sans-serif"],
+        Marcellus: ["Marcellus", "sans-serif"],
+      },
+      dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.7)",
+          "0 0px 65px rgba(255, 255,255, 0.5)",
+        ],
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
