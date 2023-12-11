3851e51e583d1e0444efa28416ae192385ee2d20
savsalmon <savannah.b.hilton@gmail.com>
2023-12-11 02:21:58 -0700

    added styling on border

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let userMood = ``
 let userMoodReason = ``
+let userMoodAdvice = `You need some time to yourself. Take a break from your phone and computer. Go outside and get some fresh air. Take a walk. Talk to a friend or family member. You can also try meditating.`
 
 // documment selectors
 let otherMoodBtn = document.getElementById("otherMoodBtn")
@@ -8,10 +9,12 @@ let moodContinueBtn = document.getElementById("moodContinueBtn")
 let otherMoodInput = document.getElementById("otherMoodInput")
 let moodBtns = document.querySelectorAll(".moodBtn")
 let moodBtnGroup = document.getElementById("moodBtnGroup")
-let submitBtn1 = document.getElementById("submitBtn1")
+let submitBtn1 = document.querySelector(".submitBtn1")
 let questionAsker = document.getElementById("questionAsker")
 let selectMoodModal = document.getElementById("selectMoodModal")
 let closeErrorBtn = document.getElementById("closeErrorBtn")
+let submitBtn2 = document.querySelector(".submitBtn2")
+let submitBtnHolder = document.getElementById("submitBtnHolder")
 
 otherMoodBtn.addEventListener("click", function (event) {
   event.stopPropagation()
@@ -43,5 +46,17 @@ submitBtn1.addEventListener("click", function (event) {
   } else {
     moodBtnGroup.innerHTML = `<textarea rows="10" cols="50" class="textarea text-area-bordered break-words min-w-2/3 h-96 rounded-2xl p-5 text-sky-50 bg-slate-900" placeholder="It was an off day, It was a good day, I went to a place... etc."></textarea>`
     questionAsker.innerHTML = `Why are you feeling ${userMood} today?`
+  }
+
+  submitBtnHolder.innerHTML = `<button class="btn btn-outline text-sky-50 mx-3 text-4xl my-12 submitBtn2">SUBMIT</button>`
+})
+
+submitBtnHolder.addEventListener("click", function (event) {
+  if (event.target.classList.contains("submitBtn2")) {
+    userMoodReason = document.querySelector(".textarea").value
+    console.log(userMoodReason)
+    submitBtnHolder.innerHTML = ``
+    questionAsker.innerHTML = `Your Caring AI Recommendation:`
+    moodBtnGroup.innerHTML = `<div type="text" class="w-5/6 text-sky-50 bg-slate-900 rounded-2xl p-5">${userMoodAdvice}</div>`
   }
 })
