1034a744819d78947048274bb53122659362c3bd
savsalmon <savannah.b.hilton@gmail.com>
2023-12-11 05:16:55 -0700

    got mood array working to populate emoji buttons

--- index.js
+++ index.js
@@ -78,11 +78,9 @@ let moods = [
 ]
 
 // documment selectors
-let otherMoodBtn = document.getElementById("otherMoodBtn")
 let otherMoodModal = document.getElementById("otherMoodModal")
 let moodContinueBtn = document.getElementById("moodContinueBtn")
 let otherMoodInput = document.getElementById("otherMoodInput")
-let moodBtns = document.querySelectorAll(".moodBtn")
 let moodBtnGroup = document.getElementById("moodBtnGroup")
 let submitBtn1 = document.querySelector(".submitBtn1")
 let questionAsker = document.getElementById("questionAsker")
@@ -94,49 +92,70 @@ let hiddenAdvice = document.getElementById("hiddenAdvice")
 
 hiddenAdvice.style.display = "none"
 
-otherMoodBtn.addEventListener("click", function (event) {
-  event.stopPropagation()
-  otherMoodModal.showModal()
+moodBtnGroup.innerHTML = ``
+moods.forEach(function (emotion) {
+  moodBtnGroup.innerHTML += `<button
+  class="moodBtn focus:drop-shadow-chosen focus:scale-110 m-3 flex flex-col drop-shadow-glow hover:drop-shadow-select transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 items-center"
+>
+  <span class="text-6xl">${emotion.htmlCode}</span
+  ><span class="font-Lexend">${emotion.mood}</span>
+</button>`
 })
+moodBtnGroup.innerHTML += `<button
+class="btn btn-outline focus:drop-shadow-chosen focus:scale-110 text-sky-50 m-3 drop-shadow-glow hover:drop-shadow-select transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+id="otherMoodBtn"
+>
+OTHER MOOD
+</button>`
 
-moodContinueBtn.addEventListener("click", function () {
-  userMood = otherMoodInput.value
-  otherMoodModal.close()
-  console.log(userMood)
-})
+document.addEventListener("DOMContentLoaded", function () {
+  let otherMoodBtn = document.getElementById("otherMoodBtn")
+  let moodBtns = document.querySelectorAll(".moodBtn")
 
-moodBtns.forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    userMood = this.children[1].innerText.toLowerCase()
+  otherMoodBtn.addEventListener("click", function (event) {
+    event.stopPropagation()
+    otherMoodModal.showModal()
+  })
+
+  moodContinueBtn.addEventListener("click", function () {
+    userMood = otherMoodInput.value
+    otherMoodModal.close()
     console.log(userMood)
   })
-})
 
-closeErrorBtn.addEventListener("click", function () {
-  selectMoodModal.close()
-})
+  moodBtns.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      userMood = this.children[1].innerText.toLowerCase()
+      console.log(userMood)
+    })
+  })
 
-submitBtn1.addEventListener("click", function (event) {
-  event.stopPropagation()
-  if (userMood === ``) {
-    selectMoodModal.showModal()
-    return
-  } else {
-    moodBtnGroup.innerHTML = `<textarea rows="10" cols="50" class="textarea text-area-bordered break-words min-w-2/3 h-96 rounded-2xl p-5 text-sky-50 bg-slate-900" placeholder="It was an off day, It was a good day, I went to a place... etc."></textarea>`
-    questionAsker.innerHTML = `Why are you feeling ${userMood} today?`
-  }
+  closeErrorBtn.addEventListener("click", function () {
+    selectMoodModal.close()
+  })
 
-  submitBtnHolder.innerHTML = `<button class="btn btn-outline text-sky-50 mx-3 text-4xl my-12 submitBtn2">SUBMIT</button>`
-})
+  submitBtn1.addEventListener("click", function (event) {
+    event.stopPropagation()
+    if (userMood === ``) {
+      selectMoodModal.showModal()
+      return
+    } else {
+      moodBtnGroup.innerHTML = `<textarea rows="10" cols="50" class="textarea text-area-bordered break-words min-w-2/3 h-96 rounded-2xl p-5 text-sky-50 bg-slate-900" placeholder="It was an off day, It was a good day, I went to a place... etc."></textarea>`
+      questionAsker.innerHTML = `Why are you feeling ${userMood} today?`
+    }
 
-submitBtnHolder.addEventListener("click", function (event) {
-  if (event.target.classList.contains("submitBtn2")) {
-    userMoodReason = document.querySelector(".textarea").value
-    console.log(userMoodReason)
-    submitBtnHolder.innerHTML = ``
-    moodBtnGroup.innerHTML = ``
-    questionAsker.innerHTML = `Your Caring AI Recommendation:`
-    hiddenAdvice.style.display = "block"
-    hiddenAdvice.firstChild.innerText = `${userMoodAdvice}`
-  }
+    submitBtnHolder.innerHTML = `<button class="btn btn-outline text-sky-50 mx-3 text-4xl my-12 submitBtn2">SUBMIT</button>`
+  })
+
+  submitBtnHolder.addEventListener("click", function (event) {
+    if (event.target.classList.contains("submitBtn2")) {
+      userMoodReason = document.querySelector(".textarea").value
+      console.log(userMoodReason)
+      submitBtnHolder.innerHTML = ``
+      moodBtnGroup.innerHTML = ``
+      questionAsker.innerHTML = `Your Caring AI Recommendation:`
+      hiddenAdvice.style.display = "block"
+      hiddenAdvice.firstChild.innerText = `${userMoodAdvice}`
+    }
+  })
 })
