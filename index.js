9fa40bc04a5edd2d495cc9ab3545e37bd93e3c69
savsalmon <savannah.b.hilton@gmail.com>
2023-12-10 20:11:38 -0700

    working to get POST for chatGPT API working

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-import "dotenv/config.js"
-import OpenAI from "openai"
-
-const openai = new OpenAI(process.env.OPENAI_API_KEY)
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://mood-mental-health-bd1224syt-savannahcode.vercel.app"
+    : "http://localhost:3000"
 
 let userMood = `angry`
 let userMoodReason = `I failed a test and finals are very stressful. I feel like there's not enough time in the day to get everything done.`
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content: `User is feeling ${userMood}. The reason they're feeling this way is ${userMoodReason}. They struggle with their mental health at varying times to varying degrees. Please advise the user with good advice. Pointing them to useful resources if necessary, for example therapists or psychiatrists in more extreme cases, but most cases to their family, friends, and trusted loved ones. Encourage solutions that get them away from being absorbed in technology online. If they are doing well, advise them on how to keep it up.`,
-      },
-    ],
-    model: "gpt-3.5-turbo",
-  })
+// documment selectors
+let otherMoodBtn = document.getElementById("otherMoodBtn")
+let otherMoodModal = document.getElementById("otherMoodModal")
 
-  console.log(completion.choices[0].message.content)
+otherMoodBtn.addEventListener("click", function () {
+  console.log("otherMoodBtn clicked")
+  otherMoodModal.showModal()
+  generateText(userMood, userMoodReason)
+    .then((text) => {
+      console.log(text)
+      otherMoodModal.textContent = text
+    })
+    .catch((error) => console.error(error))
+  //userMood = document.getElementById("otherMood").value
+})
+
+// index.js
+async function generateText(userMood, userMoodReason) {
+  const response = await fetch("http://localhost:3000/generate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userMood, userMoodReason }),
+  })
+  const text = await response.json()
+  return text
 }
 
-main()
+generateText(userMood, userMoodReason)
+  .then((text) => console.log(text))
+  .catch((error) => console.error(error))
---
+++ server.js
@@ -0,0 +1,30 @@
+// server.js
+import express from "express"
+import { OpenAI } from "openai"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+
+const app = express()
+const openai = new OpenAI(process.env.OPENAI_API_KEY)
+
+app.use(express.json())
+app.use(cors())
+
+app.post("/generate", async (req, res) => {
+  const { userMood, userMoodReason } = req.body
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `User is feeling ${userMood}. The reason they're feeling this way is ${userMoodReason}. They struggle with their mental health at varying times to varying degrees. Please advise the user with good advice. Pointing them to useful resources if necessary, for example therapists or psychiatrists in more extreme cases, but most cases to their family, friends, and trusted loved ones. Encourage solutions that get them away from being absorbed in technology online. If they are doing well, advise them on how to keep it up.`,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  })
+
+  res.json(completion.choices[0].message.content)
+})
+
+app.listen(3000, () => console.log("Server listening on port 3000"))
