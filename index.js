8139a35a012929e34dfcab315381c5093aef77cd
savsalmon <savannah.b.hilton@gmail.com>
2023-12-10 18:01:14 -0700

    got chatgpt api code working!!!

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-const { Configuration, OpenAIAPI } = require("openai")
+import "dotenv/config.js"
+import OpenAI from "openai"
 
-// Note: There is no need to use Configuration, directly use OpenAIAPI
-const openai = new OpenAIAPI({
-  key: "YOUR_API_KEY",
-})
+const openai = new OpenAI(process.env.OPENAI_API_KEY)
 
-const runPrompt = async () => {
-  const prompt = "Tell me a joke about a cat eating pasta."
+let userMood = `angry`
+let userMoodReason = `I failed a test and finals are very stressful. I feel like there's not enough time in the day to get everything done.`
 
-  const response = await openai.createCompletion({
-    engine: "text-davinci-003",
-    prompt: prompt,
-    max_tokens: 2048,
-    temperature: 1,
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `User is feeling ${userMood}. The reason they're feeling this way is ${userMoodReason}. They struggle with their mental health at varying times to varying degrees. Please advise the user with good advice. Pointing them to useful resources if necessary, for example therapists or psychiatrists in more extreme cases, but most cases to their family, friends, and trusted loved ones. Encourage solutions that get them away from being absorbed in technology online. If they are doing well, advise them on how to keep it up.`,
+      },
+    ],
+    model: "gpt-3.5-turbo",
   })
-  console.log(response.data)
+
+  console.log(completion.choices[0].message.content)
 }
 
-runPrompt()
+main()
